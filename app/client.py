a08550a898207e2cb08f4a4ba61fda9c9d013215
Avanteesh Motheram <143586258+Avanteesh@users.noreply.github.com>
2025-04-16 18:56:22 +0530

    Add files via upload

---
+++ app/client.py
@@ -0,0 +1,23 @@
+from sys import argv
+import socket
+
+
+def main():
+    HOST, PORT = ('127.0.0.1', 6379)
+    if len(argv) == 3:
+        HOST, PORT = argv[1], argv[2]
+    with socket.create_connection((HOST, PORT)) as conn:
+        while True:
+            command = input(f"{HOST}:{PORT}> ").lstrip().rstrip()
+            if command == ".exit":
+                conn.close()
+                return
+            conn.sendall(command.encode())
+            recieved = conn.recv(1024)
+            print(recieved.decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
+
+
+
